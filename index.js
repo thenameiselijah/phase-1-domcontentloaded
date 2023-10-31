4ab963962f3ed1a8acbcb5a48d3a79fae3643488
Elijah Oquendo <elijahoquendo420@gmail.com>
2023-10-31 16:33:09 -0400

    dom content loaded

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const textElement = document.querySelector("#text");
+    textElement.textContent = "This is really cool!";
+  });
